81ecde9bb7a7a84f591c6903a4066bf63fc12b68
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-15 02:17:43 +0600

    timber tree added

--- js/index.js
+++ js/index.js
@@ -186,4 +186,42 @@ const showMedical = (fruits)=>{
   }
 }
 // medical end
+// timber
+const loadTimber = ()=>{
+  fetch('https://openapi.programming-hero.com/api/plants')
+      .then(res =>res.json())
+      .then(fruit => showTimber(fruit.plants))
+}
+const showTimber = (fruits)=>{
+  const category = document.getElementById('category')
+  category.innerHTML = ''
+  for(let fruit of fruits){
+    if(fruit.category === 'Timber Tree'){
+      const fruitTree = document.createElement('div')
+      fruitTree.innerHTML = `
+      <figure>
+    <img class='h-[185px] w-[310px]'
+      src="${fruit.image}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${fruit.name}</h2>
+    <p>${fruit.description}</p>
+    <div class='flex items-center gap-7'>
+   
+    <button>${fruit.category}</button>
+
+    <p>৳${fruit.price}</p>
+    </div>
+    <div class="card-actions w-full">
+      <button class="btn btn-primary">Add to Card</button>
+    </div>
+  </div>
+</div>
+      `
+ category.appendChild(fruitTree)
+
+    }
+  }
+}
+// timber end
 loadTrees()
