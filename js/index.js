c2d9ebb6e76ec21133f205b75b14e0f8652b6c7c
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-15 02:35:59 +0600

    aquatic added

--- js/index.js
+++ js/index.js
@@ -376,4 +376,42 @@ const showClimber= (fruits)=>{
   }
 }
 // climbers end
+// aquatic
+const loadAquatic = ()=>{
+  fetch('https://openapi.programming-hero.com/api/plants')
+      .then(res =>res.json())
+      .then(fruit => showAquatic(fruit.plants))
+}
+const showAquatic = (fruits)=>{
+  const category = document.getElementById('category')
+  category.innerHTML = ''
+  for(let fruit of fruits){
+    if(fruit.category === 'Aquatic Plant'){
+      const fruitTree = document.createElement('div')
+      fruitTree.innerHTML = `
+      <figure>
+    <img class='h-[185px] w-[310px]'
+      src="${fruit.image}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${fruit.name}</h2>
+    <p>${fruit.description}</p>
+    <div class='flex items-center gap-7'>
+   
+    <button>${fruit.category}</button>
+
+    <p>৳${fruit.price}</p>
+    </div>
+    <div class="card-actions w-full">
+      <button class="btn btn-primary">Add to Card</button>
+    </div>
+  </div>
+</div>
+      `
+ category.appendChild(fruitTree)
+
+    }
+  }
+}
+// aquatic end
 loadTrees()
