ccd04067605d434f1cdc06e276bab77fe41a50dc
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-15 02:26:14 +0600

    ormental added

--- js/index.js
+++ js/index.js
@@ -262,4 +262,42 @@ const showEverGreen = (fruits)=>{
   }
 }
 // evergreen end
+// ornamental
+const loadOrnamental = ()=>{
+  fetch('https://openapi.programming-hero.com/api/plants')
+      .then(res =>res.json())
+      .then(fruit => showOrnamental(fruit.plants))
+}
+const showOrnamental = (fruits)=>{
+  const category = document.getElementById('category')
+  category.innerHTML = ''
+  for(let fruit of fruits){
+    if(fruit.category === 'Ornamental Plant'){
+      const fruitTree = document.createElement('div')
+      fruitTree.innerHTML = `
+      <figure>
+    <img class='h-[185px] w-[310px]'
+      src="${fruit.image}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${fruit.name}</h2>
+    <p>${fruit.description}</p>
+    <div class='flex items-center gap-7'>
+   
+    <button>${fruit.category}</button>
+
+    <p>৳${fruit.price}</p>
+    </div>
+    <div class="card-actions w-full">
+      <button class="btn btn-primary">Add to Card</button>
+    </div>
+  </div>
+</div>
+      `
+ category.appendChild(fruitTree)
+
+    }
+  }
+}
+// ornamental end
 loadTrees()
