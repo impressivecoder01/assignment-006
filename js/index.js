ddcd0c5a1a00a565617c9cbb299d0695cfeb338b
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-15 02:07:35 +0600

    shade shown successfully

--- js/index.js
+++ js/index.js
@@ -110,5 +110,42 @@ const showFlower =(fruits)=>{
   }
 }
 // flower end
+// Shade 
+const loadShade=()=>{
+  fetch('https://openapi.programming-hero.com/api/plants')
+      .then(res =>res.json())
+      .then(fruit => showShade(fruit.plants))
+}
+const showShade = (fruits)=>{
+  const category = document.getElementById('category')
+  category.innerHTML = ''
+  for(let fruit of fruits){
+    if(fruit.category === 'Shade Tree'){
+      const fruitTree = document.createElement('div')
+      fruitTree.innerHTML = `
+      <figure>
+    <img class='h-[185px] w-[310px]'
+      src="${fruit.image}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${fruit.name}</h2>
+    <p>${fruit.description}</p>
+    <div class='flex items-center gap-7'>
+   
+    <button>${fruit.category}</button>
 
+    <p>৳${fruit.price}</p>
+    </div>
+    <div class="card-actions w-full">
+      <button class="btn btn-primary">Add to Card</button>
+    </div>
+  </div>
+</div>
+      `
+ category.appendChild(fruitTree)
+
+    }
+  }
+}
+// Shade end
 loadTrees()
