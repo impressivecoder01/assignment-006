44734bbc5411a52be7daf0d2883bbce9190a53f8
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-15 02:21:54 +0600

    evergreen added

--- js/index.js
+++ js/index.js
@@ -224,4 +224,42 @@ const showTimber = (fruits)=>{
   }
 }
 // timber end
+// evergreen
+const loadEverGreen = ()=>{
+  fetch('https://openapi.programming-hero.com/api/plants')
+      .then(res =>res.json())
+      .then(fruit => showEverGreen(fruit.plants))
+}
+const showEverGreen = (fruits)=>{
+  const category = document.getElementById('category')
+  category.innerHTML = ''
+  for(let fruit of fruits){
+    if(fruit.category === 'Evergreen Tree'){
+      const fruitTree = document.createElement('div')
+      fruitTree.innerHTML = `
+      <figure>
+    <img class='h-[185px] w-[310px]'
+      src="${fruit.image}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${fruit.name}</h2>
+    <p>${fruit.description}</p>
+    <div class='flex items-center gap-7'>
+   
+    <button>${fruit.category}</button>
+
+    <p>৳${fruit.price}</p>
+    </div>
+    <div class="card-actions w-full">
+      <button class="btn btn-primary">Add to Card</button>
+    </div>
+  </div>
+</div>
+      `
+ category.appendChild(fruitTree)
+
+    }
+  }
+}
+// evergreen end
 loadTrees()
