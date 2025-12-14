d432ae235884cb6e4d09794afa4c8f5c14d15776
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-15 00:41:53 +0600

    showed fruit tree

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const showTrees = (plants) => {
     for (let plant of plants) {
         const aside = document.createElement('div')
         aside.innerHTML = `
-        <div class="card bg-white w-96 shadow-sm">
+        
   <figure>
     <img class='h-[185px] w-[310px]'
       src="${plant.image}" />
@@ -19,7 +19,9 @@ const showTrees = (plants) => {
     <h2 class="card-title">${plant.name}</h2>
     <p>${plant.description}</p>
     <div class='flex items-center gap-7'>
+   
     <button>${plant.category}</button>
+
     <p>৳${plant.price}</p>
     </div>
     <div class="card-actions w-full">
@@ -31,4 +33,41 @@ const showTrees = (plants) => {
         category.appendChild(aside)
     }
 }
+const loadFruit =()=>{
+      fetch('https://openapi.programming-hero.com/api/plants')
+      .then(res =>res.json())
+      .then(fruit => showFruitTree(fruit.plants))
+}
+const showFruitTree = (fruits) =>{
+  const category = document.getElementById('category')
+  category.innerHTML = ''
+  for(let fruit of fruits){
+    if(fruit.category === 'Fruit Tree'){
+      const fruitTree = document.createElement('div')
+      fruitTree.innerHTML = `
+      <figure>
+    <img class='h-[185px] w-[310px]'
+      src="${fruit.image}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${fruit.name}</h2>
+    <p>${fruit.description}</p>
+    <div class='flex items-center gap-7'>
+   
+    <button>${fruit.category}</button>
+
+    <p>৳${fruit.price}</p>
+    </div>
+    <div class="card-actions w-full">
+      <button class="btn btn-primary">Add to Card</button>
+    </div>
+  </div>
+</div>
+      `
+ category.appendChild(fruitTree)
+
+    }
+  }
+}
+
 loadTrees()
